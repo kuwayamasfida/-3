12a97119aad4dcaa616ce872ccdb05751dc6bebc
kuwayamasfida <kuwayamasfida@gmail.com>
2025-12-15 15:55:08 +0900

    22

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function handleFullscreenChange() {
     if (isFullscreen) {
         fullscreenButton.textContent = '全画面解除';
     } else {
-        fullscreenButton.textContent = '全画面20';
+        fullscreenButton.textContent = '全画面22';
     }
 }
 
