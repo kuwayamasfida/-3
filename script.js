7783a8a0122caa8ee7c1af58b01bc5dd8e86c87b
kuwayamasfida <kuwayamasfida@gmail.com>
2025-12-15 12:48:27 +0900

    tyousei

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function handleFullscreenChange() {
     if (isFullscreen) {
         fullscreenButton.textContent = '全画面解除';
     } else {
-        fullscreenButton.textContent = '全画面';
+        fullscreenButton.textContent = '1';
     }
 }
 
