a0d86dbf52df6646f20735f4545480a8fe904b76
kuwayamasfida <kuwayamasfida@gmail.com>
2025-12-15 16:22:29 +0900

    gap消した

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function handleFullscreenChange() {
     if (isFullscreen) {
         fullscreenButton.textContent = '全画面解除';
     } else {
-        fullscreenButton.textContent = '全画面27';
+        fullscreenButton.textContent = '全画面28';
     }
 }
 
